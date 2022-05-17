9b171b36e12355edec8e15680a3ba54025b3a685
shinebayar <shinebayar999@gmail.com>
2022-05-17 20:28:49 +0800

    added react-bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,51 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Alert from 'react-bootstrap/Alert';
+import { Badge } from 'react-bootstrap';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      blog goes here ....
+      <>
+        {[
+          "primary",
+          "secondary",
+          "success",
+          "danger",
+          "warning",
+          "info",
+          "light",
+          "dark",
+        ].map((variant) => (
+          <Alert key={variant} variant={variant}>
+            This is a {variant} alert with{" "}
+            <Alert.Link href="#">an example link</Alert.Link>. Give it a click
+            if you like.
+          </Alert>
+        ))}
+      </>
+
+      <div>
+  <h1>
+    Example heading <Badge bg="secondary">New</Badge>
+  </h1>
+  <h2>
+    Example heading <Badge bg="secondary">New</Badge>
+  </h2>
+  <h3>
+    Example heading <Badge bg="secondary">New</Badge>
+  </h3>
+  <h4>
+    Example heading <Badge bg="secondary">New</Badge>
+  </h4>
+  <h5>
+    Example heading <Badge bg="secondary">New</Badge>
+  </h5>
+  <h6>
+    Example heading <Badge bg="secondary">New</Badge>
+  </h6>
+</div>
     </div>
-  )
+  );
 }
