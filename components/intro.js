6e81e941143b2b5839df5b4fb1b9450ba95b1b22
shinebayar <shinebayar999@gmail.com>
2022-05-18 11:02:32 +0800

    0.0.1 version

---
+++ components/intro.js
@@ -0,0 +1,23 @@
+import { Row, Col, Image } from "react-bootstrap";
+
+export default () => (
+  <Row>
+    <Col md="1">
+      <Image
+        roundedCircle
+        width={64}
+        height={64}
+        className="mr-3"
+        src="logo.jpg"
+        alt="Generic placeholder"
+      />
+    </Col>
+    <Col md="11">
+      <h5 className="font-weight-bold mb-0">1234.mn - Онлайн видео сургалт</h5>
+      <p className="welcome-text">
+        Бид програмчлалын технолгийн чиглэлээр төрөл бүрийн сонирхолтой
+        мэдээллүүдийг энэхүү блогоор хүргэж байна.
+      </p>
+    </Col>
+  </Row>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,73 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Alert from 'react-bootstrap/Alert';
-import { Badge } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  Nav,
+  Media,
+  Image,
+  Card,
+} from "react-bootstrap";
+
+import MyNavbar from "components/my-navbar";
+import Intro from "components/intro";
+import ListItem from "components/list-item";
+import GridItem from "components/grid-item";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <>
-        {[
-          "primary",
-          "secondary",
-          "success",
-          "danger",
-          "warning",
-          "info",
-          "light",
-          "dark",
-        ].map((variant) => (
-          <Alert key={variant} variant={variant}>
-            This is a {variant} alert with{" "}
-            <Alert.Link href="#">an example link</Alert.Link>. Give it a click
-            if you like.
-          </Alert>
-        ))}
-      </>
+    <Container>
+      <MyNavbar />
+      <div className="blog-detail-page">
+        <Row>
+          <Col md="12">
+            <Intro />
+          </Col>
+        </Row>
 
-      <div>
-  <h1>
-    Example heading <Badge bg="secondary">New</Badge>
-  </h1>
-  <h2>
-    Example heading <Badge bg="secondary">New</Badge>
-  </h2>
-  <h3>
-    Example heading <Badge bg="secondary">New</Badge>
-  </h3>
-  <h4>
-    Example heading <Badge bg="secondary">New</Badge>
-  </h4>
-  <h5>
-    Example heading <Badge bg="secondary">New</Badge>
-  </h5>
-  <h6>
-    Example heading <Badge bg="secondary">New</Badge>
-  </h6>
-</div>
-    </div>
+        <hr />
+
+        <div className={`page-wrapper`}>
+          <Row className="mb-5">
+            <Col md="10">
+              <ListItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+          </Row>
+        </div>
+      </div>
+      <footer className="page-footer">
+        <div>
+          <a href="#">нүүр</a>
+          {" | "}
+          <a href="#">сургалт</a>
+          {" | "}
+          <a href="#">фэйсбүүк</a>
+        </div>
+      </footer>
+    </Container>
   );
 }
