b1631a40f0455cbceba35bc7cf19943d81171c1b
shinebayar <shinebayar999@gmail.com>
2022-05-27 18:00:49 +0800

    changed index file

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,48 @@
-import {
-  Row,
-  Col,
-} from "react-bootstrap";
-
+import { Row, Col } from "react-bootstrap";
 import GridItem from "components/grid-item";
 import { getAllPosts } from "lib/api";
 import Layout from "components/layout";
+import Intro from "components/intro";
+import { useSWRInfinite } from "swr";
+
+const PAGE_LIMIT = 3;
 
 export default function Home({ posts }) {
+  const { data, size, setSize } = useSWRInfinite(
+    (index) => `/api/posts?page=${index}&limit=${PAGE_LIMIT}`
+  );
+
   return (
     <Layout>
-      {/* <pre>{JSON.stringify(posts, 0, 2)}</pre> */}
-      <Row className="mb-5">
-        <Col md="10">{/* <ListItem /> */}</Col>
-
-        {posts.map((post) => (
-          <Col md="4" key={post.title}>
-            <GridItem post={post} />
-          </Col>
-        ))}
+      <Row>
+        <Col md="12">
+          <Intro />
+        </Col>
       </Row>
+      <hr />
+      <pre>{/*JSON.stringify(data, null, 2)*/}</pre>
+      <Row className="mb-5">
+        {data &&
+          data.map((page) =>
+            page.map((post) => (
+              <Col md={12 / PAGE_LIMIT}>
+                <GridItem post={post} />
+              </Col>
+            ))
+          )}
+      </Row>
+      <div style={{ textAlign: "center" }}>
+        {data && data[data.length - 1].length !== 0 && (
+          <button onClick={() => setSize(size + 1)}>Цааш нь ...</button>
+        )}
+      </div>
     </Layout>
   );
 }
 
 export const getStaticProps = async () => {
-  const posts = await getAllPosts();
+  const posts = await getAllPosts(1, PAGE_LIMIT);
+
   return {
     props: {
       posts,
